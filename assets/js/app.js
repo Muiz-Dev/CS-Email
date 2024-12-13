c87829172d902f825551e8a43ac25b26e2fa5a7a
Adesopequizzify <adesope@quizzify.com.ng>
2024-12-13 12:11:38 +0100

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,15 +8,6 @@ function emailApp() {
     attachment: null,
     sending: false,
 
-    init() {
-      tinymce.init({
-        selector: '#content',
-        plugins: 'link image code',
-        toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | link image | code',
-        height: 300
-      });
-    },
-
     loadTemplate() {
       if (this.selectedTemplate) {
         fetch(`templates/${this.selectedTemplate}.html`)
@@ -24,7 +15,7 @@ function emailApp() {
           .then(template => {
             // Extract the content section from the template
             const contentSection = template.match(/<td style="padding: 30px; color: #d1d1d1; text-align: left;">([\s\S]*?)<\/td>/)[1];
-            tinymce.get('content').setContent(contentSection);
+            this.content = contentSection;
           })
           .catch(error => {
             console.error('Error loading template:', error);
@@ -43,7 +34,7 @@ function emailApp() {
       formData.append('to', this.to);
       formData.append('subject', this.subject);
       formData.append('recipientName', this.recipientName);
-      formData.append('content', tinymce.get('content').getContent());
+      formData.append('content', this.content);
       formData.append('template', this.selectedTemplate);
       if (this.attachment) {
         formData.append('attachment', this.attachment);
@@ -75,9 +66,10 @@ function emailApp() {
       this.subject = '';
       this.recipientName = '';
       this.selectedTemplate = '';
-      tinymce.get('content').setContent('');
+      this.content = '';
       this.attachment = null;
       document.getElementById('attachment').value = '';
     }
   };
 }
+
